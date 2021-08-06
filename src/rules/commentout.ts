97c78b80167e662f8ba08f3243b5c8ebbfceef39
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2021-08-07 05:01:37 +0900

    feat:allow annotation comment

--- src/rules/commentout.ts
+++ src/rules/commentout.ts
@@ -19,18 +19,25 @@ export const commentout: TSESLint.RuleModule<'commentout', []> = {
   create: (
     context: Readonly<TSESLint.RuleContext<'commentout', []>>,
   ): TSESLint.RuleListener => {
-    const existsComemntout = (line: string): boolean => {
-      return new RegExp('^// .*').test(line)
-    }
-
     return {
       Program: (node: TSESTree.Node): void => {
         context.getSourceCode().lines.forEach((line: string): void => {
-          if (!existsComemntout(line)) return
+          if (isValid(line)) return
 
           context.report({ node, messageId: 'commentout' })
         })
       },
     }
   },
+}
+
+const isValid = (line: string): boolean => {
+  const hasCommentout = new RegExp('^// .*').test(line)
+  if (!hasCommentout) return true
+
+  const hasAnnotationComment = new RegExp(
+    '^// (TODO|FIXME|HACK|XXX|NOTE|WARNING:)|@.*',
+  ).test(line)
+
+  return hasCommentout && hasAnnotationComment
 }
