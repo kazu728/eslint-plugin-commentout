82b81f8a9493be61b057f5ca9ce4e9fdae5084e6
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2022-06-18 20:12:08 +0900

    refactor: added MESSAGE_ID constant

--- src/rules/commentout.ts
+++ src/rules/commentout.ts
@@ -4,7 +4,10 @@ import {
   TSESTree,
 } from '@typescript-eslint/experimental-utils'
 
-export const commentout: TSESLint.RuleModule<'commentout', []> = {
+const MESSAGE_ID = 'commentout'
+type MESSAGE_ID = typeof MESSAGE_ID
+
+export const commentout: TSESLint.RuleModule<MESSAGE_ID, []> = {
   meta: {
     type: 'suggestion',
     docs: {
@@ -20,13 +23,10 @@ export const commentout: TSESLint.RuleModule<'commentout', []> = {
   },
 
   create: (
-    context: Readonly<TSESLint.RuleContext<'commentout', []>>,
+    context: Readonly<TSESLint.RuleContext<MESSAGE_ID, []>>,
   ): TSESLint.RuleListener => {
     const report = (node: TSESTree.Comment): void =>
-      context.report({
-        node,
-        messageId: 'commentout',
-      })
+      context.report({ node, messageId: MESSAGE_ID })
 
     return {
       Program: (): void => {
@@ -41,9 +41,8 @@ export const commentout: TSESLint.RuleModule<'commentout', []> = {
 }
 
 const isInvalid = (node: TSESTree.Comment): boolean => {
-  const hasAnnotationComment = new RegExp(
-    '(TODO|FIXME|HACK|XXX|NOTE|WARNING).*|@.*',
-  ).test(node.value)
+  const pattern = '(TODO|FIXME|HACK|XXX|NOTE|WARNING).*|@.*'
+  const hasAnnotationComment = new RegExp(pattern).test(node.value)
 
   return node.type === AST_TOKEN_TYPES.Line && !hasAnnotationComment
 }
